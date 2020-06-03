1eb9b973c787c0fb70b7afd96b87be80d15d7ad0
unknown <kwameagyenimboateng64@gmail.com>
2020-06-03 02:47:47 +0000

    initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\pdfs\school\third year 2nd sem\cscd 312 into to ai\project\age_estimation_by_voice\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
